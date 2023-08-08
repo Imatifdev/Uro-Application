7b7f8c2b77d22aeeee2707006345f08c7e357512
Muhammad Atif Pervaiz <matifpervaizofficial@gmail.com>
2023-08-08 09:56:41 +0500

    oaf quiz added

--- lib/view/patient/oab.dart
+++ lib/view/patient/oab.dart
@@ -1,12 +1,9 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:uroapplication/view/calculator/quiz2_screen.dart';
 import 'package:uroapplication/view/calculator/quiz_screen.dart';
 import '../../controller/mycolors.dart';
-
 import 'package:page_transition/page_transition.dart';
 import 'package:swipeable_button_view/swipeable_button_view.dart';
 
@@ -39,9 +36,17 @@ class _OABScreenState extends State<OABScreen> {
     print(userId);
   }
 
-  // int _currentIndex = 0;
-
-  // final List<Widget> _screens = [];
+  LinearGradient getProgressBarGradient(double percentage) {
+    // Your gradient color logic here
+    // Example:
+    if (percentage <= 0.4) {
+      return LinearGradient(colors: [Colors.green.shade900, Colors.green]);
+    } else if (percentage < 0.7) {
+      return LinearGradient(colors: [Colors.orange, Colors.yellow.shade300]);
+    } else {
+      return LinearGradient(colors: [Colors.orange.shade700, Colors.red]);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,172 +54,185 @@ class _OABScreenState extends State<OABScreen> {
     final screenHeight = MediaQuery.of(context).size.height;
     final textScaleFactor = MediaQuery.of(context).textScaleFactor;
 
-    // Adjust font size based on screen width and text scale factor
-    //final fontSize = screenWidth * 0.14 * textScaleFactor;
     final subheading = screenWidth * 0.06 * textScaleFactor;
     final heading = screenWidth * 0.08 * textScaleFactor;
 
+    // Determine the text direction to set the alignment of the slide buttons
+    final isRTL = Directionality.of(context) == TextDirection.rtl;
+    final slideButtonAlignment =
+        isRTL ? Alignment.centerRight : Alignment.centerLeft;
+
     return Scaffold(
       backgroundColor: Colors.white,
-        body: Container(
-      height: screenHeight,
-      width: screenWidth,
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [skyblue, darkblue],
+      body: Container(
+        height: screenHeight,
+        width: screenWidth,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [skyblue, darkblue],
+          ),
+        ),
+        child: Column(
+          children: [
+            SizedBox(
+              height: screenHeight * 0.09,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Health Test: Quick Assessment for Vital Indicators.",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: subheading, color: Colors.white),
+              ),
+            ),
+            SizedBox(
+              height: screenHeight * 0.04,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Container(
+                height: screenHeight / 3,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.white.withOpacity(0.2),
+                ),
+                child: Column(
+                  children: [
+                    Column(
+                      children: [
+                        Image(
+                          height: screenHeight / 7,
+                          width: screenWidth,
+                          image: AssetImage('assets/images/1.png'),
+                        ),
+                        Text(
+                          "OAB Diagnosis",
+                          style: TextStyle(
+                            fontSize: heading,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: screenHeight / 30,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: SwipeableButtonView(
+                        buttonText: 'Start OAB Diagnosis',
+                        buttonWidget: Container(
+                          child: Icon(
+                            Icons.arrow_forward_ios_rounded,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        activeColor: Colors.white.withOpacity(0.2),
+                        isFinished: isFinished,
+                        onWaitingProcess: () {
+                          Future.delayed(Duration(seconds: 2), () {
+                            setState(() {
+                              isFinished = true;
+                            });
+                          });
+                        },
+                        onFinish: () async {
+                          await Navigator.push(
+                            context,
+                            PageTransition(
+                              type: PageTransitionType.fade,
+                              child: OAB(),
+                            ),
+                          );
+
+                          //TODO: For reverse ripple effect animation
+                          setState(() {
+                            isFinished = false;
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: screenHeight * 0.01),
+            //quiz2
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Container(
+                height: screenHeight / 3,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.white.withOpacity(0.2),
+                ),
+                child: Column(
+                  children: [
+                    Column(
+                      children: [
+                        Image(
+                          height: screenHeight / 7,
+                          width: screenWidth,
+                          image: AssetImage('assets/images/2.png'),
+                        ),
+                        Text(
+                          "IPSS Diagnosis",
+                          style: TextStyle(
+                            fontSize: heading,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: screenHeight / 30,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: SwipeableButtonView(
+                        buttonText: 'Start IPSS Diagnosis',
+                        buttonWidget: Container(
+                          child: Icon(
+                            Icons.arrow_forward_ios_rounded,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        activeColor: Colors.white.withOpacity(0.2),
+                        isFinished: isFinished,
+                        onWaitingProcess: () {
+                          Future.delayed(Duration(seconds: 2), () {
+                            setState(() {
+                              isFinished = true;
+                            });
+                          });
+                        },
+                        onFinish: () async {
+                          await Navigator.push(
+                            context,
+                            PageTransition(
+                              type: PageTransitionType.fade,
+                              child: IPSS(),
+                            ),
+                          );
+
+                          //TODO: For reverse ripple effect animation
+                          setState(() {
+                            isFinished = false;
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
-      child: Column(
-        children: [
-          SizedBox(
-            height: screenHeight * 0.09,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              "Health Test: Quick Assessment for Vital Indicators.",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: subheading, color: Colors.white),
-            ),
-          ),
-          SizedBox(
-            height: screenHeight * 0.04,
-          ),
-
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Container(
-              height: screenHeight / 3,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.white.withOpacity(0.2)),
-              child: Column(
-                children: [
-                  Column(
-                    children: [
-                      Image(
-                        height: screenHeight / 7,
-                        width: screenWidth,
-                        image: AssetImage('assets/images/1.png'),
-                      ),
-                      Text(
-                        "OAB Diagnosis",
-                        style: TextStyle(
-                            fontSize: heading,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white),
-                      )
-                    ],
-                  ),
-                  SizedBox(
-                    height: screenHeight / 30,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: SwipeableButtonView(
-                      buttonText: 'Start OAB Diagnosis',
-                      buttonWidget: Container(
-                        child: Icon(
-                          Icons.arrow_forward_ios_rounded,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      activeColor: Colors.white.withOpacity(0.2),
-                      isFinished: isFinished,
-                      onWaitingProcess: () {
-                        Future.delayed(Duration(seconds: 2), () {
-                          setState(() {
-                            isFinished = true;
-                          });
-                        });
-                      },
-                      onFinish: () async {
-                        await Navigator.push(
-                            context,
-                            PageTransition(
-                                type: PageTransitionType.fade, child: OAB()));
-
-                        //TODO: For reverse ripple effect animation
-                        setState(() {
-                          isFinished = false;
-                        });
-                      },
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-          SizedBox(height: screenHeight * 0.01),
-          //quiz2
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Container(
-              height: screenHeight / 3,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.white.withOpacity(0.2)),
-              child: Column(
-                children: [
-                  Column(
-                    children: [
-                      Image(
-                        height: screenHeight / 7,
-                        width: screenWidth,
-                        image: AssetImage('assets/images/2.png'),
-                      ),
-                      Text(
-                        "IPSS Diagnosis",
-                        style: TextStyle(
-                            fontSize: heading,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white),
-                      )
-                    ],
-                  ),
-                  SizedBox(
-                    height: screenHeight / 30,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: SwipeableButtonView(
-                      buttonText: 'Start IPSS Diagnosis',
-                      buttonWidget: Container(
-                        child: Icon(
-                          Icons.arrow_forward_ios_rounded,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      activeColor: Colors.white.withOpacity(0.2),
-                      isFinished: isFinished,
-                      onWaitingProcess: () {
-                        Future.delayed(Duration(seconds: 2), () {
-                          setState(() {
-                            isFinished = true;
-                          });
-                        });
-                      },
-                      onFinish: () async {
-                        await Navigator.push(
-                            context,
-                            PageTransition(
-                                type: PageTransitionType.fade, child: IPSS()));
-
-                        //TODO: For reverse ripple effect animation
-                        setState(() {
-                          isFinished = false;
-                        });
-                      },
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    ));
+    );
   }
 }
